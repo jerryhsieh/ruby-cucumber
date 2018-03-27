04d7b1fd2d875cd252a24c07eeed5a2e3be33726
Jerry Hsieh <jerryhsieh01@gmail.com>
2018-03-27 11:46:24 +0800

    modify mina config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,6 @@
 #require 'mina/rails'
+require 'mina/deploy'
+require 'mina/bundler'
 require 'mina/git'
 require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
 # require 'mina/rvm'    # for rvm support. (https://rvm.io)
@@ -8,18 +10,22 @@ require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
 #   deploy_to    - Path to deploy into.
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
-set :domain, ''
-
-set :application_name, 'foobar'
-set :domain, 'foobar.com'
-set :deploy_to, '/var/www/foobar.com'
-set :repository, 'git://...'
+set :application_name, 'liked'
+set :domain, '172.28.128.6'
+set :deploy_to, '/var/apps/liked'
+set :repository, 'https://github.com/jerryhsieh/ruby-cucumber.git'
 set :branch, 'master'
 
 # Optional settings:
 #   set :user, 'foobar'          # Username in the server to SSH to.
 #   set :port, '30000'           # SSH port number.
 #   set :forward_agent, true     # SSH forward_agent.
+
+set :user, 'liked'
+set :forward_agent, true
+set :identity_file, '/Users/jerryhsieh/.ssh/id_rsa'
+set :rbenv_path, '/home/vagrant/.rbenv/bin'
+
 
 # Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
 # Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
@@ -36,26 +42,30 @@ task :remote_environment do
 
   # For those using RVM, use this to load an RVM version@gemset.
   # invoke :'rvm:use', 'ruby-1.9.3-p125@default'
+  invoke :'rbenv:load'
+  #command "export PATH=/opt/rbenv/bin:/opt/rbenv/shims:$PATH"
 end
 
 # Put any custom commands you need to run at setup
 # All paths in `shared_dirs` and `shared_paths` will be created on their own.
-task :setup do
+task :setup => :remote_environment  do
   # command %{rbenv install 2.3.0 --skip-existing}
+  command "mkdir -p #{fetch(:deploy_to)}/current/tmp/pids #{fetch(:deploy_to)}/current/tmp/sockets"
+  command "mkdir -p #{fetch(:deploy_to)}/current/log"
 end
 
 desc "Deploys the current version to the server."
-task :deploy do
+task :deploy => :remote_environment do
   # uncomment this line to make sure you pushed your local branch to the remote origin
   # invoke :'git:ensure_pushed'
   deploy do
     # Put things that will set up an empty directory into a fully set-up
     # instance of your project.
     invoke :'git:clone'
-    invoke :'deploy:link_shared_paths'
+    #invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
-    invoke :'rails:db_migrate'
-    invoke :'rails:assets_precompile'
+    #invoke :'rails:db_migrate'
+    #invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
     on :launch do
